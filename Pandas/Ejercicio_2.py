040430d15973bdf18285ffb627d1dfe79609ed8e
Clementeqp <clementeqp@hotmail.com>
2021-09-08 09:42:52 +0200

    corregir readme

---
+++ Pandas/Ejercicio_2.py
@@ -0,0 +1,9 @@
+
+
+""" Escribir una función que reciba un diccionario con las notas de los alumnos 
+en curso en un examen y devuelva una serie con la nota mínima, la máxima, 
+media y la desviación típica. """
+
+calification_alumns = {
+    
+}
